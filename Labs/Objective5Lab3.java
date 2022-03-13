ac082a45120e07fc996d50b14281a41c8f4fa38e
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-03-13 16:29:38 -0400

    complete objective5lab3

---
+++ Labs/Objective5Lab3.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Objective5Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int userNum;
+
+    System.out.println("Please enter an integer number: ");
+
+    userNum = scanner.nextInt();
+    if (userNum > 0) {
+      System.out.println("The number is greater than 0.");
+    }
+    else if (userNum < 0) {
+      System.out.println("The number is less than 0.");
+    }
+    else {
+      System.out.println("The number is 0.");
+    }
+
+    scanner.close();
+  }
+}
