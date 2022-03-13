c16f67693ea8dfb4cec42c905c87a2981995b093
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-03-13 17:00:20 -0400

    complete objective5Lab4

---
+++ Labs/Objective5Lab4.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Objective5Lab4 {
+  public static void main(String [] args) {
+    
+    Scanner scanner = new Scanner(System.in);
+
+    int num;
+
+    System.out.println("Please enter an integer number: ");
+
+    num = scanner.nextInt();
+
+    if(num % 2 == 0) {
+
+      System.out.println("The number is even.");
+    }
+
+    else {
+
+      System.out.println("The number is odd.");
+    }
+
+    scanner.close();
+  }
+
+}
