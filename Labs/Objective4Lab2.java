3a4df0bfd16d7aa0e4c07d12d7442dedf4360b8e
jdhuf <91219279+jdhuf@users.noreply.github.com>
2022-03-12 22:35:58 -0500

    complete objective4lab2

--- Labs/Objective4Lab2.java
+++ Labs/Objective4Lab2.java
@@ -31,5 +31,7 @@ public class Objective4Lab2 {
 
     System.out.println("The sum of " + num1 + " + " + num2 + " + " + num3 + " = " + sum1);
     System.out.println("The sum of " + dub1 + " + " + dub2 + " + " + dub3 + " = " + sum2);
+
+    keyboard.close();
   }
 }
